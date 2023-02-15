3629b0782226f4febe4d4bd9d27c9919f8f1d308
ibrahim serhan <ibrahimserhan29@gmail.com>
2023-02-16 01:31:01 +0200

    add coversation migration

--- database/migrations/2023_02_15_231431_create_conversations_table.php
+++ database/migrations/2023_02_15_231431_create_conversations_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('conversations', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user1_id');
+            $table->unsignedBigInteger('user2_id');
+            $table->foreign('user1_id')->references('id')->on('users');
+            $table->foreign('user2_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
