792cd6a8ab9cae79d62739b068ad1de0327aac19
ibrahim serhan <ibrahimserhan29@gmail.com>
2023-02-18 07:19:34 +0200

    add bootstrap

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client'
+import Login from './pages/Login';
+import 'bootstrap/dist/css/bootstrap.css';
 import '../css/app.css'
 export default function App(){
     return(
-        <h1>How To Install React in Laravel aa with Vite</h1>
+        <Login />
     );
 }
 
