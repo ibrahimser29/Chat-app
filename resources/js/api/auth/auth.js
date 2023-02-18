0cd3333c31faedc6b377566779c0d131a6858a20
ibrahim serhan <ibrahimserhan29@gmail.com>
2023-02-19 00:26:13 +0200

    change content type header for register

--- resources/js/api/auth/auth.js
+++ resources/js/api/auth/auth.js
@@ -1,7 +1,7 @@
 import api from '../api';
 
 export const SendRegisterRequest = (data) => {
-    return api.post('/register',data);
+    return api.post('/register',data,{headers:{"Content-Type": "multipart/form-data"}});
 }
 
 export const SendLoginRequest = (cardentials) => {
