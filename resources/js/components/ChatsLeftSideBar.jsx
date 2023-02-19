9f44727c5bc45c2f5147492383fa8adc5240a2ac
ibrahim serhan <ibrahimserhan29@gmail.com>
2023-02-19 03:59:58 +0200

    handle Retrive users/conversations

--- resources/js/components/ChatsLeftSideBar.jsx
+++ resources/js/components/ChatsLeftSideBar.jsx
@@ -1,82 +1,63 @@
 import React from 'react'
 
-function ChatsLeftSideBar() {
+function ChatsLeftSideBar(props) {
   return (
-    <div class="col-xl-4 col-lg-4 col-md-4 col-sm-3 col-3">
-                            <div class="users-container">
-                                <div class="chat-search-box">
-                                    <div class="input-group">
-                                        <input class="form-control" placeholder="Search"/>
-                                        <div class="input-group-btn">
-                                            <button type="button" class="btn btn-info">
-                                                <i class="fa fa-search"></i>
-                                            </button>
-                                        </div>
+    <div className="col-xl-4 col-lg-4 col-md-4 col-sm-3 col-3">
+                            {props.errors != null && props.errors != undefined ?
+                            <div>{props.errors}</div> : props.conversations.length == 0 ? (<div className="users-container">
+                            <div className="chat-search-box">
+                                <div className="input-group">
+                                    <input className="form-control" placeholder="Search"/>
+                                    <div className="input-group-btn">
+                                        <button type="button" className="btn btn-info">
+                                            <i className="fa fa-search"></i>
+                                        </button>
                                     </div>
                                 </div>
-                                <ul class="users scrollContainer">
-                                    <li class="person" data-chat="person1">
-                                        <div class="user">
-                                            <img src="https://www.bootdey.com/img/Content/avatar/avatar3.png" alt="Retail Admin"/>
-                                            <span class="status busy"></span>
-                                        </div>
-                                        <p class="name-time">
-                                            <span class="name">Steve Bangalter</span>
-                                            <span class="time">15/02/2019</span>
-                                        </p>
-                                    </li>
-                                    <li class="person" data-chat="person1">
-                                        <div class="user">
-                                            <img src="https://www.bootdey.com/img/Content/avatar/avatar1.png" alt="Retail Admin"/>
-                                            <span class="status offline"></span>
-                                        </div>
-                                        <p class="name-time">
-                                            <span class="name">Steve Bangalter</span>
-                                            <span class="time">15/02/2019</span>
-                                        </p>
-                                    </li>
-                                    <li class="person active-user" data-chat="person2">
-                                        <div class="user">
-                                            <img src="https://www.bootdey.com/img/Content/avatar/avatar2.png" alt="Retail Admin"/>
-                                            <span class="status away"></span>
-                                        </div>
-                                        <p class="name-time">
-                                            <span class="name">Peter Gregor</span>
-                                            <span class="time">12/02/2019</span>
-                                        </p>
-                                    </li>
-                                    <li class="person" data-chat="person3">
-                                        <div class="user">
-                                            <img src="https://www.bootdey.com/img/Content/avatar/avatar3.png" alt="Retail Admin"/>
-                                            <span class="status busy"></span>
-                                        </div>
-                                        <p class="name-time">
-                                            <span class="name">Jessica Larson</span>
-                                            <span class="time">11/02/2019</span>
-                                        </p>
-                                    </li>
-                                    <li class="person" data-chat="person4">
-                                        <div class="user">
-                                            <img src="https://www.bootdey.com/img/Content/avatar/avatar4.png" alt="Retail Admin"/>
-                                            <span class="status offline"></span>
-                                        </div>
-                                        <p class="name-time">
-                                            <span class="name">Lisa Guerrero</span>
-                                            <span class="time">08/02/2019</span>
-                                        </p>
-                                    </li>
-                                    <li class="person" data-chat="person5">
-                                        <div class="user">
-                                            <img src="https://www.bootdey.com/img/Content/avatar/avatar5.png" alt="Retail Admin"/>
-                                            <span class="status away"></span>
-                                        </div>
-                                        <p class="name-time">
-                                            <span class="name">Michael Jordan</span>
-                                            <span class="time">05/02/2019</span>
-                                        </p>
-                                    </li>
-                                </ul>
                             </div>
+                            <ul className="users scrollContainer">
+                                <h4>Suggestions</h4>
+                                {props.users.map((user,i)=>{
+                                    return  <li key={i} className="person" data-chat="person1">
+                                    <div className="user">
+                                        <img src={`/images/${user.image}`} alt="Retail Admin"/>
+                                        <span className="status busy"></span>
+                                    </div>
+                                    <p className="name-time">
+                                        <span className="name">{user.name}</span>
+                                        <span className="time">15/02/2019</span>
+                                    </p>
+                                </li>
+                                })}
+                            </ul>
+                        </div>) : (<div className="users-container">
+                            <div className="chat-search-box">
+                                <div className="input-group">
+                                    <input className="form-control" placeholder="Search"/>
+                                    <div className="input-group-btn">
+                                        <button type="button" className="btn btn-info">
+                                            <i className="fa fa-search"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                            <ul className="users scrollContainer">
+                                {props.users.map((user,i)=>{
+                                    return  <li key={i} className="person" data-chat="person1">
+                                    <div className="user">
+                                        <img src={`/images/${user.image}`} alt="Retail Admin"/>
+                                        <span className="status busy"></span>
+                                    </div>
+                                    <p className="name-time">
+                                        <span className="name">{user.name}</span>
+                                        <span className="time">15/02/2019</span>
+                                    </p>
+                                </li>
+                                })}
+                            </ul>
+                        </div>)
+                            }
+                            
                         </div>
   )
 }
--- resources/js/pages/Chats.jsx
+++ resources/js/pages/Chats.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import ChatsLeftSideBar from '../components/ChatsLeftSideBar'
 import ChatsRightSideBar from '../components/ChatsRightSideBar'
-
+import { useNavigate } from "react-router-dom";
+import { sendRetrieveUsersRequest } from '../api/conversation/conversation'
 function Chats() {
+    const navigate = useNavigate();
+    const [users,setUsers] = useState([]);
+    const [conversations,setConversations] = useState([]);
+    const [errors,setErrors] = useState({});
+    const getUsersAndConvetsations = () => {
+        sendRetrieveUsersRequest().then((resp)=>{
+            setUsers(resp.data.data.users)
+            setConversations(resp.data.data.conversations)
+        }).catch((error)=>{
+            setErrors({...error,users:error.response.data.message})
+        })
+    }
+    useEffect(()=>{
+        if(localStorage.getItem('user') == null || localStorage.getItem('user') == undefined){
+            navigate('/')
+        }
+        getUsersAndConvetsations();
+    },[])
   return (
     <div class="container">
-
-
     <div class="page-title">
         <div class="row gutters">
             <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
@@ -26,7 +43,8 @@ function Chats() {
                 <div class="card m-0">
 
                     <div class="row no-gutters">
-                        <ChatsLeftSideBar />
+                        <ChatsLeftSideBar users={users} conversations={conversations}
+                         errors={errors.users} />
                         <ChatsRightSideBar />
                     </div>
                 </div>
